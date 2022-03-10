b541f6f941182285cdd174eb707566cb9ca52423
francisco-ricardo-dos-santos-silva <ricardo.ufersa.pdf@gmail.com>
2022-03-10 18:27:01 -0300

    Adicionado comentarios ao codigo e o executavel

--- inverterNumero.cpp
+++ inverterNumero.cpp
@@ -8,17 +8,15 @@ int main (){
     
     printf("Digite uma sequencia de numeros: ");	//solicitando ao ususraio um numero
     scanf("%i", &numero);			//recebe o numero digitado e armazena na respectiva variavel
-    if (numero >= 0){	
-        			//se numero > 0 entra nessa condicao
+    if (numero >= 0){				//se numero > 0 entra nessa condicao
         do {
-			cifra = numero % 10;
-			printf("%i", cifra);	
-			numero /= 10;
-		} while (numero != 0);   
-
+			cifra = numero % 10;	//a variavel cifra recebe o resto da divisao do numero por 10
+			printf("%i", cifra);	//printa o numero na tela a cada execucao
+			numero /= 10;			//numero recebe o numero dividido por 10
+		} while (numero != 0);		//enquanto o resto da divisao for diferente de zero a funcao sera executada.
 	}
 	else {		//se o numero for menor que zero imprime a mensagem de numero invalido
-		printf("Numero inavlido, tente um numero positivo :'(");
+		printf("Numero invalido, tente um numero positivo :'(");
 	}
     	
 	return 0;
